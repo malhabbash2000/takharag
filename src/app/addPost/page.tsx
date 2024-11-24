4a25518cc14df320169c1ab3994d4cd1e59b6730
Majd Zreed <mjdzreed@gmail.com>
2024-11-24 19:58:11 +0300

    feature finish

--- src/app/addPost/page.tsx
+++ src/app/addPost/page.tsx
@@ -1,41 +1,45 @@
 import styles from "../page.module.css";
 import Header from "../components/Header";
-import PrimaryImage from "../components/PrimaryImage";
 import Link from "next/link";
 import React from 'react'
 
 function addPost() {
   return (
-    <div className={styles.background}>
-        <Header></Header>
-      <form method="post">
-        <legend>أضف مشروعك للبيع</legend>
-        <div className="mb-4">
+    <div className={`${"flex"} ${"justify-center"} ${"flex-col"} ${"items-center"}`}>
+      <Header></Header>
+    <div className={`${styles.background} ${"flex"} ${"justify-center"} ${"flex-col"} ${"items-center"}`}>
+      <form method="post" className={`${styles["form-add"]}`}>
+        <legend className={`${styles["title-form"]}`}>أضف مشروعك للبيع</legend>
+        
+        <div className="m-4">
           {/* proj name */}
             <label htmlFor="projName"></label>
             <input className={styles.input} type="text" id="projName" name="projName" placeholder="عنوان المشروع" />
             <br />
             {/* city */}
+            <div className="flex flex-row gap-5" >
             <label htmlFor="city"></label>
-            <input type="text" id="city" name="city" placeholder="المدينة" />
+            <input className={`${styles.input} w-1/2 h-12`} type="text" id="city" name="city" placeholder="المدينة" />
             <br />
             {/* price */}
             <label htmlFor="price"></label>
-            <input type="text" id="price" name="price" placeholder="السعر المقترح" />
+            <input className={`${styles.input} w-1/2 h-12`} type="text" id="price" name="price" placeholder="السعر المقترح" />
             <br />
+            </div>
             {/* projDesc */}
             <label htmlFor="projDesc"></label>
-            <input type="text" id="projDesc" name="projDesc" placeholder="وصف المشروع" />
+            <input className={`${styles.input} ${"projDesc"}`} type="text" id="projDesc" name="projDesc" placeholder="وصف المشروع" />
             <br />
             {/* sellRes */}
             <label htmlFor="sellRes"></label>
-            <input type="text" id="sellRes" name="sellRes" placeholder="سبب البيع" />
+            <input className={`${styles.input} ${"sellRes"}`} type="text" id="sellRes" name="sellRes" placeholder="سبب البيع" />
             <br />
             {/* pic */}
             <label htmlFor="pic"></label>
-            <input type="text" id="pic" name="pic" placeholder="إضافة صور للمشروع" />
+            <input className={`${styles.input} ${"pic"}`} type="text" id="pic" name="pic" placeholder="إضافة صور للمشروع" />
         </div>
       </form>
+    </div>
     </div>
   )
 }
